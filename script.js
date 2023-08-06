d747e394b452fa5f2479d68dca7caa9f4910afc5
DanielAagaard <42737491+DanielAagaard@users.noreply.github.com>
2023-08-07 00:33:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ const catData = [
     {
         name: '#1 Sort/hvid (Hun)',
         images: ['billeder/h_1_1.jpg', 'billeder/h_1_2.jpg', 'billeder/h_1_3.jpg', 'billeder/h_1_4.jpg'],
-        description: "Født d. 28 maj 2023 (Ca. 10 uger gammel)<br>Klar til afhentning d. 20 august 2023<br>Ormekur givet d. 15 juli 2023<br><span>Pris: 100 kr.</span>"
+        description: "Født d. 28 maj 2023 (Ca. 10 uger)<br>Klar til afhentning d. 20 august 2023<br>Ormekur givet d. 15 juli 2023<br><span>Pris: 100 kr.</span>"
     },
 	{
         name: '#2 Sort/hvid (Han)',
         images: ['billeder/h_3_1.jpg', 'billeder/h_3_2.jpg', 'billeder/h_3_3.jpg', 'billeder/h_3_4.jpg'],
-        description: "Født d. 28 maj 2023 (Ca. 10 uger gammel)<br>Klar til afhentning d. 20 august 2023<br>Ormekur givet d. 15 juli 2023<br><span>Pris: 100 kr.</span>"
+        description: "Født d. 28 maj 2023 (Ca. 10 uger)<br>Klar til afhentning d. 20 august 2023<br>Ormekur givet d. 15 juli 2023<br><span>Pris: 100 kr.</span>"
     },
 	{
         name: '#3 Sort/grå (Hun)',
         images: ['billeder/h_2_1.jpg', 'billeder/h_2_2.jpg', 'billeder/h_2_3.jpg', 'billeder/h_2_4.jpg'],
-        description: "Født d. 28 maj 2023 (Ca. 10 uger gammel)<br>Klar til afhentning d. 20 august 2023<br>Ormekur givet d. 15 juli 2023<br><span>Pris: 100 kr.</span>"
+        description: "Født d. 28 maj 2023 (Ca. 10 uger)<br>Klar til afhentning d. 20 august 2023<br>Ormekur givet d. 15 juli 2023<br><span>Pris: 100 kr.</span>"
     },
 	{
         name: '#4 Sort (Han)',
         images: ['billeder/b_1_1.jpg', 'billeder/b_1_2.jpg', 'billeder/b_1_3.jpg', 'billeder/b_1_4.jpg'],
-        description: "Født d. 26 maj 2023 (Ca. 10 uger gammel)<br>Klar til afhentning d. 18 august 2023<br>Ormekur givet d. 15 juli 2023<br><span>Pris: 100 kr.</span>"
+        description: "Født d. 26 maj 2023 (Ca. 10 uger)<br>Klar til afhentning d. 18 august 2023<br>Ormekur givet d. 15 juli 2023<br><span>Pris: 100 kr.</span>"
     },
 	{
         name: '#5 Sort (Han)',
         images: ['billeder/b_1_7.jpg', 'billeder/b_1_6.jpg', 'billeder/b_1_5.jpg', 'billeder/b_1_8.jpg'],
-        description: "Født d. 26 maj 2023 (Ca. 10 uger gammel)<br>Klar til afhentning d. 18 august 2023<br>Ormekur givet d. 15 juli 2023<br><span>Pris: 100 kr.</span>"
+        description: "Født d. 26 maj 2023 (Ca. 10 uger)<br>Klar til afhentning d. 18 august 2023<br>Ormekur givet d. 15 juli 2023<br><span>Pris: 100 kr.</span>"
     },
     // Add more cat data here
 ];
